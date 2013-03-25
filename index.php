a6916ad17f2d5cd5cc9a56e330b971041984055a
Adam <adam@adambard.com>
2013-03-25 10:47:33 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include '_header.php'; ?>
+
+<h1>Nurblizer</h1>
+<form action="/nurble.php" method="post">
+    <fieldset>
+        <ul>
+            <li>
+                <label>Text to nurblize</label>
+                <textarea name="text"></textarea>
+            </li>
+            <li>
+                <input type="submit" value="Nurblize Away!">
+            </li>
+        </ul>
+    </fieldset>
+</form>
+<p>
+    <a href="http://www.smbc-comics.com/?id=2779">wtf?</a>
+</p>
+
+<?php include '_footer.php';
---
+++ nurble.php
@@ -0,0 +1,30 @@
+<?php
+
+$nouns = file("nouns.txt", FILE_IGNORE_NEW_LINES);
+
+function nurble($text){
+    $text = strtoupper($text);
+    $words = preg_split(
+        '/\w/',
+        preg_replace('/[^a-z ]/', '', strtolower($text)));
+
+    foreach($words as $word){
+        if(in_array($word, $nouns)){
+            $pattern = '/(\b)' . $word . '(\b)/i';
+            $replacement = '\1<span class="nurble">nurble</span>\2';
+            $text = preg_replace($pattern, $replacement, $text);
+        }
+    }
+
+    return str_replace("\n", '<br>', $text);
+}
+
+include '_header.php'; ?>
+
+<h1>Your Nurbled Text</h1>
+<div><?php echo nurble($_POST['text']); ?></div>
+<p>
+    <a href="/">&lt;&lt; Back</a>
+</p>
+
+<?php include '_footer.php';
